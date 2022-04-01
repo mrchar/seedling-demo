3d6b9f47826b45f5f5064f777783dac499dd69bb
mrchar <mrchar@qq.com>
2022-04-01 14:31:46 +0800

    初始化项目

---
+++ src/main/java/com/example/seedling/SeedlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.seedling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seedling/SeedlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seedling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
