19ae857ff01e261204241d52df6d83f6b223a0bf
mrchar <mrchar@qq.com>
2022-04-01 14:35:49 +0800

    添加Hello World接口

--- src/main/java/com/example/seedling/SeedlingApplication.java
+++ src/main/java/com/example/seedling/SeedlingApplication.java
@@ -2,12 +2,19 @@ package com.example.seedling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SeedlingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SeedlingApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SeedlingApplication.class, args);
+  }
 
+  @RequestMapping("/**")
+  public String greeting() {
+    return "hello world";
+  }
 }
